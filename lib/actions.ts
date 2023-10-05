54b9f2a4ef62dc7ab03f77302c707e0250be4c62
0xNunana <kudayapaul@gmail.com>
2023-10-05 13:45:10 +0000

    Api key

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,7 +3,7 @@ import React from "react";
 import { Resend } from "resend"
 import ContactFormEmail from "@/components/ContactFormEmail";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 export const getErrorMessage = (error: unknown): string => {
     let message: string;
   
