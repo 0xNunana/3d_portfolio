f9f2f4705cc4411673dc5fff52341a448a57578a
0xNunana <kudayapaul@gmail.com>
2023-10-08 23:45:33 +0000

    more projects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -5,6 +5,7 @@ import SectionTitle from './SectionTitle'
 import { projectsData } from '@/lib/data'
 import ProjectCard from './ProjectCard'
 import { NavViewer } from '@/lib/navView'
+import Link from 'next/link'
 
 const Projects = () => {
  const {ref}=NavViewer('Projects',0.3)
@@ -13,9 +14,12 @@ const Projects = () => {
   
   return (
     <section className='mb-5 sm:mb-20 scroll-mt-20 sm:scroll-mt-28' id='projects' ref={ref}>
-        <SectionTitle>My Projects</SectionTitle>
+        <SectionTitle>Recent Projects</SectionTitle>
         {projectsData.map((pro,i)=>(<div key={i} className=''>
+            <Link href={pro.href} target='_blank'>
             <ProjectCard {...pro} />
+            </Link>
+       
         </div>
           
         ))}
